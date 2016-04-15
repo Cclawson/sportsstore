ea415186c4077e17ff39a1ca677ea508e2e7cf5b
Cody Clawson <cclawson@student.neumont.edu>
2016-04-15 13:12:26 -0600

    chapter 7 done

---
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entitites;
+using SportStore.WebUI.Models;
+
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repsitory;
+        public int PageSize = 4;
+        // GET: Product
+        public ProductController(IProductRepository productRepository) 
+        {
+            this.repsitory = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repsitory.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repsitory.Products.Count()
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SportStore.WebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entitites;
+using SportStore.WebUI.Models;
+
+namespace SportStore.WebUI.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
